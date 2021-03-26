51051b8e8934ecd08ee9373cca446e82ce6aa79c
David Polcari <dpolcari18@gmail.com>
2021-03-26 10:22:05 -0500

    added relationships and seed data

--- flatiron_eats/app/models/restaurant.rb
+++ flatiron_eats/app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+    has_many :likes
+    has_many :reviews
+    has_many :users, through: :likes
 end
--- flatiron_eats/db/seeds.rb
+++ flatiron_eats/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'pry'
+
+# i = 0 
+# users = []
+# while i < 5 do 
+#     users.push(Faker::Name.unique.name)
+#     i++
+# end
+
+Like.destroy_all
+Review.destroy_all
+User.destroy_all
+Restaurant.destroy_all
+
+
+cities = []
+5.times {cities.push(Faker::Address.city)}
+
+5.times { User.create(name: Faker::Name.unique.name, email: Faker::Internet.email, city: cities.sample(1)[0])}
+
+20.times {Restaurant.create(name: Faker::Company.name, location: cities.sample(1)[0], website: Faker::Internet.domain_name)}
+
+Like.create(user_id: User.all.first.id, restaurant_id: Restaurant.all.first.id)
+
+Review.create(user_id: User.all.first.id, restaurant_id: Restaurant.all.first.id, content: Faker::Hipster.sentence, rating: 3)
