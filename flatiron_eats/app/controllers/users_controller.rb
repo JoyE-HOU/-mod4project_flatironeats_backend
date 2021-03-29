40a2ad3da81b2c345f309c948574d002d5ebcac7
David Polcari <dpolcari18@gmail.com>
2021-03-29 09:14:29 -0500

    added create action for user

--- flatiron_eats/app/controllers/users_controller.rb
+++ flatiron_eats/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create 
+        user = User.new(user_params)
+        user.save
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :city)
+    end
 end
