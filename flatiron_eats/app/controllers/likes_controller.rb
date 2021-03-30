dc4c1c93b280d2406787c707ccc01557f2fd6503
David Polcari <dpolcari18@gmail.com>
2021-03-29 19:45:59 -0500

    added delete action for likes

--- flatiron_eats/app/controllers/likes_controller.rb
+++ flatiron_eats/app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+    def create
+        like = Like.new(like_params)
+        like.save
+        render json: like
+    end
+
+    def destroy
+        like = Like.find_by(id: params[:id])
+        like.destroy
+        render json: like
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:restaurant_id, :user_id)
+    end
 end
